b03b93799c20684d600742659f2bc089bb7f0ad2
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-19 15:55:59 -0300

    cria base para project controller

--- Portfolio-Pessoal-InterativoAPI/Portfolio-Pessoal-InterativoAPI/Controllers/ProjectController.cs
+++ Portfolio-Pessoal-InterativoAPI/Portfolio-Pessoal-InterativoAPI/Controllers/ProjectController.cs
@@ -15,16 +15,31 @@ namespace Portfolio_Pessoal_InterativoAPI.Controllers
             _projectHandler = projectHandler;
         }
 
-        [HttpGet("home")]
+        [HttpGet]
         public async Task<IActionResult> HomePage()
         {
-            return Ok("Home Page com todos os projetos criados");
+            var result = await _projectHandler.GetAllProjectsHandler();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProjectById(Guid id)
+        {
+            var result = await _projectHandler.GetProjectHandler(id);
+            return Ok(result);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateNewProject(ProjectDTO projectDTO)
+        public async Task<IActionResult> CreateNewProject([FromBody] ProjectDTO projectDTO)
         {
             var result = await _projectHandler.CreateNewProjectHandler(projectDTO);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProject(Guid id, [FromBody] ProjectDTO projectDTO)
+        {
+            var result = await _projectHandler.UpdateProjectHandler(id,projectDTO);
             return Ok(result);
         }
     }
