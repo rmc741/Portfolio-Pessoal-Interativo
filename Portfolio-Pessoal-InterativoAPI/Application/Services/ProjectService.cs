82f6e139ac9d568013de1f9f6c62dcb465596606
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-19 16:52:26 -0300

    classe de service para project e sua interface.

---
+++ Portfolio-Pessoal-InterativoAPI/Application/Services/ProjectService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ProjectService : IProjectService
+    {
+    }
+}
