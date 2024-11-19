0ed67c3b31b6ec781dcf009db0fa70d2323550c6
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-19 16:52:48 -0300

    remove code smells.

--- Portfolio-Pessoal-InterativoAPI/Application/Interfaces/Services/IProjectService.cs
+++ Portfolio-Pessoal-InterativoAPI/Application/Interfaces/Services/IProjectService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Interfaces.Services
+﻿namespace Application.Interfaces.Services
 {
     public interface IProjectService
     {
--- Portfolio-Pessoal-InterativoAPI/Application/Services/ProjectService.cs
+++ Portfolio-Pessoal-InterativoAPI/Application/Services/ProjectService.cs
@@ -1,9 +1,4 @@
 ﻿using Application.Interfaces.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Services
 {
