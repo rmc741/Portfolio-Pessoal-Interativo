7d5dcc166cdf5f934f091fb7a64bde05075f4440
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-19 08:46:05 -0300

    criação da camada de Application, Domain, Infra e Ioc

---
+++ Portfolio-Pessoal-InterativoAPI/IoC/DependecyInjectionApi.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+{
+    public static class DependecyInjectionApi
+    {
+        public static IServiceCollection AddInfrastructureApi(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddOptionsApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
