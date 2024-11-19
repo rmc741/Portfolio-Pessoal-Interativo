9f934fa1e40e016c48203b9f456e6572e83865fb
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-19 10:33:52 -0300

    interface e classe de project handler

---
+++ Portfolio-Pessoal-InterativoAPI/Application/Handlers/ProjectHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Handlers
+{
+    public class ProjectHandler : IProjectHandler
+    {
+        public Task<ProjectDTO> CreateNewProjectHandler(ProjectDTO projectDTO)
+        {
+            throw new NotImplementedException("Criação de projeto ainda não implementada!!");
+        }
+
+        public Task DeleteProjectHandler(Guid id)
+        {
+            throw new NotImplementedException("Função ainda não implementada!");
+        }
+
+        public Task<ProjectDTO> UpdateProjectHandler(ProjectDTO projectDTO)
+        {
+            throw new NotImplementedException("Função ainda não implementada!");
+        }
+    }
+}
---
+++ Portfolio-Pessoal-InterativoAPI/Application/Interfaces/IProjectHandler.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IProjectHandler
+    {
+        Task<ProjectDTO> CreateNewProjectHandler(ProjectDTO projectDTO);
+        Task<ProjectDTO> UpdateProjectHandler(ProjectDTO projectDTO);
+        Task DeleteProjectHandler(Guid id);
+    }
+}
