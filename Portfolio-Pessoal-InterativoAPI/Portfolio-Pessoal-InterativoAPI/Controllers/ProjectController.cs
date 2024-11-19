8e942d33ac78295275e5cc3bb22dc467abc5123b
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-19 10:33:10 -0300

    project controller.

---
+++ Portfolio-Pessoal-InterativoAPI/Portfolio-Pessoal-InterativoAPI/Controllers/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Pessoal_InterativoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectHandler _projectHandler;
+
+        public ProjectController(IProjectHandler projectHandler)
+        {
+            _projectHandler = projectHandler;
+        }
+
+        [HttpGet("home")]
+        public async Task<IActionResult> HomePage()
+        {
+            return Ok("Home Page com todos os projetos criados");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewProject(ProjectDTO projectDTO)
+        {
+            var result = await _projectHandler.CreateNewProjectHandler(projectDTO);
+            return Ok(result);
+        }
+    }
+}
