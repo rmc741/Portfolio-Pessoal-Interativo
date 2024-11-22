ba2662f423a4aca8b634ad53e9d49e2382f21f4c
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-22 16:35:36 -0300

    base do project service

--- Portfolio-Pessoal-InterativoAPI/Application/Services/ProjectService.cs
+++ Portfolio-Pessoal-InterativoAPI/Application/Services/ProjectService.cs
@@ -1,8 +1,33 @@
-﻿using Application.Interfaces.Services;
+﻿using Application.DTOs;
+using Application.Interfaces.Services;
 
 namespace Application.Services
 {
     public class ProjectService : IProjectService
     {
+        public Task<ProjectDTO> CreateNewProject(ProjectDTO projectDTO)
+        {
+            throw new NotImplementedException("Não implementado serviço para criar projeto");
+        }
+
+        public Task<List<ProjectDTO>> GetAllProjectsAsync()
+        {
+            throw new NotImplementedException("Função para pegar todos os projetos ainda não implementado");
+        }
+
+        public Task<ProjectDTO> GetProjectByIdAsync(Guid id)
+        {
+            throw new NotImplementedException("Função para buscar por id ainda não implementado");
+        }
+
+        public Task<ProjectDTO> UpdateProjectAsync(Guid id, ProjectDTO projectDTO)
+        {
+            throw new NotImplementedException("Função para editar projeto por id ainda não implementado");
+        }
+
+        public Task DeleteProjectAsync(Guid id)
+        {
+            throw new NotImplementedException("Função para remover projeto ainda n implementado");
+        }
     }
 }
