f4b4e9b8db7ab83f92374b2460a372c6a8877492
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-22 16:34:39 -0300

    funções que será implementada no service

--- Portfolio-Pessoal-InterativoAPI/Application/Interfaces/Services/IProjectService.cs
+++ Portfolio-Pessoal-InterativoAPI/Application/Interfaces/Services/IProjectService.cs
@@ -1,6 +1,13 @@
-﻿namespace Application.Interfaces.Services
+﻿using Application.DTOs;
+
+namespace Application.Interfaces.Services
 {
     public interface IProjectService
     {
+        Task<List<ProjectDTO>> GetAllProjectsAsync();
+        Task<ProjectDTO> GetProjectByIdAsync(Guid id);
+        Task<ProjectDTO> CreateNewProject(ProjectDTO projectDTO);
+        Task<ProjectDTO> UpdateProjectAsync(Guid id, ProjectDTO projectDTO);
+        Task DeleteProjectAsync(Guid id);
     }
 }
