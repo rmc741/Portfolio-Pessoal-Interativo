83fb5b88f030f37207b3f51d91c2b75f20ba1ef3
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-25 15:25:58 -0300

    profile para dto e commands

---
+++ Portfolio-Pessoal-InterativoAPI/Application/Mappings/DTOToCommandMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.CQRS.Commands;
+using Application.DTOs;
+using AutoMapper;
+
+namespace Application.Mappings
+{
+    public class DTOToCommandMappingProfile : Profile
+    {
+        public DTOToCommandMappingProfile()
+        {
+            CreateMap<ProjectDTO, ProjectCreateCommand>();
+            CreateMap<ProjectDTO, ProjectUpdateCommand>();
+        }
+    }
+}
