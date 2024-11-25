de4b2cfffe4250d547c88c86322519b997043760
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-25 15:25:07 -0300

    commands para project

---
+++ Portfolio-Pessoal-InterativoAPI/Application/CQRS/Commands/ProjectCreateCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Application.CQRS.Commands
+{
+    public class ProjectCreateCommand
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Url]
+        [Required]
+        public string UrlGit { get; set; }
+        public HashSet<string>? Tags { get; set; } = new();
+
+        [JsonIgnore]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Portfolio-Pessoal-InterativoAPI/Application/CQRS/Commands/ProjectUpdateCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.CQRS.Commands
+{
+    public class ProjectUpdateCommand
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Url]
+        [Required]
+        public string UrlGit { get; set; }
+        public HashSet<string> Tags { get; set; } = new();
+        public List<ImageDTO>? ImagesList { get; set; } = new();
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
