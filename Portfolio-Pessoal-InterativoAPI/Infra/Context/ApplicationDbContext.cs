15ee08d82a22cfddd0fdd1caed5ac27dc7fc56a8
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-26 14:05:10 -0300

    classe de contexto para bd

---
+++ Portfolio-Pessoal-InterativoAPI/Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Infra.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options){ }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
