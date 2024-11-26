3a54a66472ba7c891bfa88f541fbf1d5e2a49bdc
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-26 14:05:43 -0300

    configuração do contexto do bd

--- Portfolio-Pessoal-InterativoAPI/IoC/DependecyInjectionApi.cs
+++ Portfolio-Pessoal-InterativoAPI/IoC/DependecyInjectionApi.cs
@@ -4,7 +4,9 @@ using Application.Interfaces.Services;
 using Application.Mappings;
 using Application.Services;
 using Domain.Interface.Repository;
+using Infra.Context;
 using Infra.Repository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,8 +14,12 @@ namespace IoC
 {
     public static class DependecyInjectionApi
     {
-        public static IServiceCollection AddInfrastructureApi(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureApi(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
             services.AddSingleton<IProjectHandler, ProjectHandler>();
             services.AddSingleton<IProjectService, ProjectService>();
             services.AddSingleton<IProjectRepository, ProjectRepository>();
