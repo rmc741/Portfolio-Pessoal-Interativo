5b5bd4e45d7a1a61227f8f6d99e55c183c67239e
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-26 15:12:48 -0300

    remove code smells

--- Portfolio-Pessoal-InterativoAPI/Infra/Context/ApplicationDbContext.cs
+++ Portfolio-Pessoal-InterativoAPI/Infra/Context/ApplicationDbContext.cs
@@ -1,12 +1,11 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Infra.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options){ }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Project> Projects { get; set; }
         public DbSet<Comment> Comments { get; set; }
