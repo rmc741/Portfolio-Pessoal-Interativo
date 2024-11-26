46de083b5777bdfdaef8fef0dee347ad0271d47b
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-26 15:12:57 -0300

    entity configuration

---
+++ Portfolio-Pessoal-InterativoAPI/Infra/EntitiesConfiguration/ProjectConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.EntitiesConfiguration
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Name).IsRequired();
+            builder.Property(e => e.Description).IsRequired();
+            builder.Property(e => e.UrlGit).IsRequired();
+            builder.Property(e => e.Tags).IsRequired();
+            builder.Property(e => e.CreatedAt);
+            builder.Property(e => e.UpdatedAt);
+
+        }
+    }
+}
+
