9818f1da5bd789cf4f8d133b36127e73d4a347d3
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-11-27 11:51:46 -0300

    remove classe n usada e renomeia para melhor entendimento

---
+++ Portfolio-Pessoal-InterativoAPI/Application/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.CQRS.Commands;
+using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Project, ProjectDTO>().ReverseMap();
+
+            CreateMap<ProjectCreateCommand, ProjectDTO>();
+            CreateMap<ProjectUpdateCommand, ProjectDTO>();
+        }
+    }
+}
