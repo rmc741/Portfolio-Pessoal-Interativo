61fc32e576d963f472743551aa15ec42c2979930
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-06 15:10:57 -0300

    comments repository e sua interface

---
+++ Portfolio-Pessoal-InterativoAPI/Domain/Interface/Repository/ICommentRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interface.Repository
+{
+    public interface ICommentRepository : IGenericRepository<Comment>
+    {
+    }
+}
---
+++ Portfolio-Pessoal-InterativoAPI/Infra/Repository/CommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Domain.Interface.Repository;
+using Infra.Context;
+
+namespace Infra.Repository
+{
+    public class CommentRepository : GenericRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
