96399f3f63590a36495a49aa0f7038b8856667d1
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-06 15:07:50 -0300

    interface e classe para comment handler junto com sua implementação

---
+++ Portfolio-Pessoal-InterativoAPI/Application/Handlers/CommentHandler.cs
@@ -0,0 +1,46 @@
+﻿using Application.CQRS.Commands;
+using Application.DTOs;
+using Application.Interfaces.Handlers;
+using Application.Interfaces.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Handlers
+{
+    public class CommentHandler : ICommentHandler
+    {
+        private readonly ICommentService _commentService;
+        private readonly ILogger<CommentHandler> _logger;
+
+        public CommentHandler(ICommentService commentService, ILogger<CommentHandler> logger)
+        {
+            _commentService = commentService;
+            _logger = logger;
+        }
+
+        public async Task<CommentDTO> CreateNewCommentHandler(CommentCreateCommand commentRequest)
+        {
+            return await _commentService.CreateNewComment(commentRequest);
+        }
+
+        public async Task DeleteCommentHandler(Guid id)
+        {
+            await _commentService.DeleteComment(id);
+        }
+
+        //talvez não precise dessa função
+        public async Task<List<CommentDTO>> GetAllCommentsByProjectIdHandler(Guid projectId)
+        {
+            return await _commentService.GetAllCommentsByProjectId(projectId);
+        }
+
+        public async Task<CommentDTO> GetCommentHandler(Guid id)
+        {
+            return await _commentService.GetCommentById(id);
+        }
+
+        public Task<CommentDTO> UpdateCommentHandler(CommentUpdateCommand commentRequest)
+        {
+            return _commentService.UpdateComment(commentRequest);
+        }
+    }
+}
--- Portfolio-Pessoal-InterativoAPI/Application/Interfaces/Handlers/ICommentHandler.cs
+++ Portfolio-Pessoal-InterativoAPI/Application/Interfaces/Handlers/ICommentHandler.cs
@@ -1,6 +1,14 @@
-﻿namespace Application.Interfaces.Handlers
+﻿using Application.CQRS.Commands;
+using Application.DTOs;
+
+namespace Application.Interfaces.Handlers
 {
     public interface ICommentHandler
     {
+        Task<CommentDTO> CreateNewCommentHandler(CommentCreateCommand commentRequest);
+        Task<CommentDTO> UpdateCommentHandler(CommentUpdateCommand commentRequest);
+        Task DeleteCommentHandler(Guid id);
+        Task<CommentDTO> GetCommentHandler(Guid id);
+        Task<List<CommentDTO>> GetAllCommentsByProjectIdHandler(Guid projectId);
     }
 }
