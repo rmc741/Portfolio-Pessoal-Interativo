cc835d456ffc771a0501b4b37e6230d7be061926
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-06 15:10:35 -0300

    relacionamento entre comments e project

--- Portfolio-Pessoal-InterativoAPI/Domain/Entities/Comment.cs
+++ Portfolio-Pessoal-InterativoAPI/Domain/Entities/Comment.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
         //public Guid UserId { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
