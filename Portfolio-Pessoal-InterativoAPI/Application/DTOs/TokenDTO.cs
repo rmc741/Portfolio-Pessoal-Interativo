7e1d16465612faa9f731a3710c81c09bcc565c28
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-10 16:42:10 -0300

    token entity e dto

--- Portfolio-Pessoal-InterativoAPI/Application/DTOs/TokenDTO.cs
+++ Portfolio-Pessoal-InterativoAPI/Application/DTOs/TokenDTO.cs
@@ -2,5 +2,8 @@
 {
     public class TokenDTO
     {
+        public string AccessToken { get; set; }
+        public string TokenType { get; set; }
+        public int ExpiresIn { get; set; }
     }
 }
