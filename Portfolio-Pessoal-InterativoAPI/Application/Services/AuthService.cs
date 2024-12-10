7119a9976c1579201bfeb2ab1e12b89a3e11c72c
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-10 10:50:55 -0300

    base de auth service e sua interface

---
+++ Portfolio-Pessoal-InterativoAPI/Application/Services/AuthService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces.Services;
+using Domain.Interface.Repository;
+
+namespace Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthRepositorty _authRepository;
+
+        public AuthService(IAuthRepositorty authRepository)
+        {
+            _authRepository = authRepository;
+        }
+    }
+}
