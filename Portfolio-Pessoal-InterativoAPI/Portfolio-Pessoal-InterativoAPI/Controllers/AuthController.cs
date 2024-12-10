2cb3db6f41a5e7c6360ddff709da234290b1d6c2
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-10 10:54:21 -0300

    controller para autenticação.

--- Portfolio-Pessoal-InterativoAPI/Portfolio-Pessoal-InterativoAPI/Controllers/AuthController.cs
+++ Portfolio-Pessoal-InterativoAPI/Portfolio-Pessoal-InterativoAPI/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.DTOs;
+using Application.Interfaces.Handlers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Pessoal_InterativoAPI.Controllers
@@ -7,5 +8,23 @@ namespace Portfolio_Pessoal_InterativoAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthHandler _authHandler;
+
+        public AuthController(IAuthHandler authHandler)
+        {
+            _authHandler = authHandler;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginRequest)
+        {
+            var user = await _authHandler.Login(loginRequest);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+
+            return BadRequest("Autenticação Falhou!");
+        }
     }
 }
