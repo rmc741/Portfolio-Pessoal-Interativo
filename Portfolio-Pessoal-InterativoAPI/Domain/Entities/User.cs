72600f1e0449284bd87095dacc675e0ee1176893
Rafael Mendonça de Carvalho <rafael.medonca2@gmail.com>
2024-12-10 10:51:55 -0300

    entitdade user

---
+++ Portfolio-Pessoal-InterativoAPI/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public bool IsActive { get; set; }
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
