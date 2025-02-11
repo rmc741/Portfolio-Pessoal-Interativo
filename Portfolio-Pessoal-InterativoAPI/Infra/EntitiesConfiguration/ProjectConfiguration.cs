394a5e8ac34f6d3b322e857702acf2c1ce491f78
Rafael Mendonca <rafael.medonca2@gmail.com>
2025-02-11 19:57:03 -0300

    comparer para hash de tags

--- Portfolio-Pessoal-InterativoAPI/Infra/EntitiesConfiguration/ProjectConfiguration.cs
+++ Portfolio-Pessoal-InterativoAPI/Infra/EntitiesConfiguration/ProjectConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System.Text.Json;
 
@@ -13,12 +14,21 @@ namespace Infra.EntitiesConfiguration
             builder.Property(e => e.Name).IsRequired();
             builder.Property(e => e.Description).IsRequired();
             builder.Property(e => e.UrlGit).IsRequired();
+
+            var comparer = new ValueComparer<HashSet<string>>(
+            (c1, c2) => c1.SetEquals(c2),  // Compara os elementos da coleção
+            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())), // Gera hash para a coleção
+            c => new HashSet<string>(c) // Garante que a cópia seja independente
+            );
+
             builder.Property(e => e.Tags)
                 .IsRequired()
                 .HasConversion(
                     v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null), // Para JSON
                     v => JsonSerializer.Deserialize<HashSet<string>>(v, (JsonSerializerOptions)null) // De JSON
-                );
+                )
+                .Metadata.SetValueComparer(comparer); // Adiciona o Value Comparer
+
             builder.Property(e => e.CreatedAt);
             builder.Property(e => e.UpdatedAt);
 
